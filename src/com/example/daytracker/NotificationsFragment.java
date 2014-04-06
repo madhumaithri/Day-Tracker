3f15c45cbd79bc5c6468616dd7d61c85f7717755
madhumaithri <madhumaithri@gmail.com>
2014-04-07 03:27:13 +0800

    Notif UI done. Next is Export,icons,about

--- src/com/example/daytracker/NotificationsFragment.java
+++ src/com/example/daytracker/NotificationsFragment.java
@@ -1,6 +1,9 @@
 package com.example.daytracker;
 
+import java.util.Calendar;
+
 import android.app.Fragment;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,8 +13,14 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.app.TimePickerDialog;
+import android.widget.TimePicker;
 
 public class NotificationsFragment extends Fragment implements OnItemSelectedListener  {
+
+	private int hour;
+	private int minute;
 
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,13 +43,34 @@ public class NotificationsFragment extends Fragment implements OnItemSelectedLis
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 	    super.onActivityCreated(savedInstanceState);
+	   
+	    TextView textCategory = (TextView) getActivity().findViewById(R.id.notifications_text);      
+	    Typeface font = Typeface.createFromAsset(getActivity().getAssets(), "fonts/Roboto-BoldItalic.ttf");
+        textCategory.setTypeface(font);
+        
+        TimePicker timePicker1 = (TimePicker) getActivity().findViewById(R.id.timePicker1);
+        
+		final Calendar c = Calendar.getInstance();
+		hour = c.get(Calendar.HOUR_OF_DAY);
+		minute = c.get(Calendar.MINUTE);
+ 
+		
+		// set current time into timepicker
+		timePicker1.setCurrentHour(hour);
+		timePicker1.setCurrentMinute(minute);
+	    
+	    /*
 	    Spinner nSpinner = (Spinner) getActivity().findViewById(R.id.notifications_spinner);
 	    nSpinner.setOnItemSelectedListener(this);
 		ArrayAdapter<CharSequence> nAdapter = ArrayAdapter.createFromResource(this.getActivity(),
 		        R.array.notifications_numbers, android.R.layout.simple_spinner_item);
 		nAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		nSpinner.setAdapter(nAdapter);
+		*/
 
+        TextView textCategoryStar = (TextView) getActivity().findViewById(R.id.ratingstars_number);      
+	    textCategoryStar.setTypeface(font);
+        
 		Spinner rSpinner = (Spinner) getActivity().findViewById(R.id.ratingstars_spinner);
 		ArrayAdapter<CharSequence> rAdapter = ArrayAdapter.createFromResource(this.getActivity(),
 			    R.array.rmd_numOfStars, android.R.layout.simple_spinner_item);
